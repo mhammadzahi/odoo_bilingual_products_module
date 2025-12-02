c07400d83dd6b6d881b975ee078c79088026641c
Mohamed <mohamedzahi33@gmail.com>
2025-12-02 20:44:04 +0400

    .

--- bilingual_product/models/product_template.py
+++ bilingual_product/models/product_template.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields, api
+from odoo import models, fields, api, _
 
 
 class ProductTemplate(models.Model):
@@ -20,6 +20,70 @@ class ProductTemplate(models.Model):
         help="A description of the Product that you want to communicate to your customers. "
              "This description will be copied to every Sales Order, Delivery Order and Customer Invoice/Credit Note"
     )
+
+    # Virtual fields for Arabic translations (used for CSV import)
+    name_ar = fields.Char(
+        'Name (Arabic)',
+        compute='_compute_name_ar',
+        inverse='_inverse_name_ar',
+        store=False,
+        help="Arabic translation of product name"
+    )
+
+    description_sale_ar = fields.Text(
+        'Sales Description (Arabic)',
+        compute='_compute_description_sale_ar',
+        inverse='_inverse_description_sale_ar',
+        store=False,
+        help="Arabic translation of sales description"
+    )
+
+    def _get_arabic_lang_code(self):
+        """Get the installed Arabic language code"""
+        Lang = self.env['res.lang']
+        # Try common Arabic language codes, starting with 'ar'
+        for code in ['ar', 'ar_001', 'ar_SY', 'ar_SA', 'ar_EG']:
+            if Lang.search([('code', '=', code), ('active', '=', True)], limit=1):
+                return code
+        return None
+
+    @api.depends('name')
+    def _compute_name_ar(self):
+        """Get Arabic translation of name"""
+        lang_code = self._get_arabic_lang_code()
+        for record in self:
+            if lang_code:
+                record.name_ar = record.with_context(lang=lang_code).name
+            else:
+                record.name_ar = False
+
+    def _inverse_name_ar(self):
+        """Set Arabic translation of name"""
+        lang_code = self._get_arabic_lang_code()
+        if not lang_code:
+            return  # Skip if Arabic not installed
+        for record in self:
+            if record.name_ar:
+                record.with_context(lang=lang_code).name = record.name_ar
+
+    @api.depends('description_sale')
+    def _compute_description_sale_ar(self):
+        """Get Arabic translation of description"""
+        lang_code = self._get_arabic_lang_code()
+        for record in self:
+            if lang_code:
+                record.description_sale_ar = record.with_context(lang=lang_code).description_sale
+            else:
+                record.description_sale_ar = False
+
+    def _inverse_description_sale_ar(self):
+        """Set Arabic translation of description"""
+        lang_code = self._get_arabic_lang_code()
+        if not lang_code:
+            return  # Skip if Arabic not installed
+        for record in self:
+            if record.description_sale_ar:
+                record.with_context(lang=lang_code).description_sale = record.description_sale_ar
 
     # Note: Fields that remain non-translatable by default:
     # - default_code (Internal Reference)
